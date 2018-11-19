ce6936f8d32c0e3b1393074492f6597d100ea6f5
Cory Routledge <croutledge@aais.com>
2018-11-19 13:47:07 -0600

    Updated exports to expose the snapshot received model

--- source/index.ts
+++ source/index.ts
@@ -1,3 +1,6 @@
 export * from './DataIngestionApi';
 export * from './IGlobalSettingsApiModel';
 export * from './ITenantSettingsApiModel';
+export * from './AstraResponse';
+export * from './SnapshotReceivedEventModel';
+export * from './ISnapshotReceivedEventModel';
